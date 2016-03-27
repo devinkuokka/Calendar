ff46112064e9cd525505860664ef7aa7846836ee
Devin Kuokka <devin.kuokka@wustl.edu>
2016-03-27 14:53:22 -0500

    Login works. Need to figure out how to redirect without redirecting

---
+++ login.php
@@ -0,0 +1,38 @@
+<?php		
+
+	if (!isset ($_POST['username']) || !isset ($_POST['password'])) {		//checks a username & password were entered
+		echo "Please enter all fields";
+		exit;
+	}
+	$username = $_POST['username'];			
+	$password = $_POST['password'];
+				
+				
+	//conect to cal database as php_user
+	require 'php_database.php';
+				
+	$isUser = $mysqli -> prepare ("select username, password
+								  from users
+								  where username = '$username' and password = '$password'");
+				
+	if (!$isUser) {
+		echo "Select Query Prep Failed: %s\n", $mysqli -> error;
+		exit;
+	}
+				
+	$isUser -> execute();
+	$isUser -> bind_result($usernameResult, $passwordResult);
+	$isUser -> fetch();
+	$isUser -> close();
+				
+	//check that username and password are valid 
+	if ($usernameResult == null && password_verify($password, $passwordResult)) {
+		echo "Invalid Login. Please try again.";
+		exit;
+	}
+	
+	echo "login successful";
+				
+	$_SESSION['username'] = $username;				//creates and sets the session variable, username
+	
+?>
--- userButtons.js
+++ userButtons.js
@@ -2,6 +2,16 @@ $(document).ready(function() {
 	$("#loginSubmit").click(function(){
 		var userVal = $('#loginUser').val();
 		var passVal = $('#loginPass').val();
+		$.ajax({
+            type: "POST",
+            url: "login.php", //This is the current doc
+            //dataType:'json', // add json datatype to get json
+            data: { username: userVal, password: passVal},
+            //data: {data: userVal},
+            success: function(msg){
+                alert(msg);
+            }
+        });
 	});
 
 	$("#signupSubmit").click(function(){
