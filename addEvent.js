588191a9776b04484e22462261caaa1935afec5f
Carolyn Dean Wolf <cdeanwolf@wustl.edu>
2016-03-28 02:19:37 -0500

    fixed date and time to sql

--- addEvent.js
+++ addEvent.js
@@ -1,7 +1,7 @@
 $(window).load(function(){
 	$('table td').on('click',function(){
 		$("#addEvent").modal("show");
-		var cellId = ($(this).closest('td').attr('id')).split('.');
+		var cellId = ($(this).closest('td').attr('id')).split('-');
 		
 		var year = cellId[0];
 		var month = ("0" + cellId[1]).slice(-2);
@@ -14,26 +14,26 @@ $(window).load(function(){
 
 	$('#eventSubmit').click(function(){
 		//var ecreator = $('#session_username').text();
-		var ecreator = "cdeanwolf";
+		//var ecreator = "cdeanwolf";
 		var ename = $('#eventName').val();
 		var edate = $('#eventDate').val();
 		var ecat = $('#eventCat').val();
 		var estart = $('#eventStartTime').val();
 		var eend = $('#eventEndTime').val();
 		
-		if (ecreator == "") {
-            alert("Please login or make an account");
-        } else {
+//		if (ecreator == "") {
+//            alert("Please login or make an account");
+//        } else {
 			$.ajax({
 				type: "POST",
 				url: "addEventScript.php",
 				//dataType:'json', // add json datatype to get json
-				data: { creator: ecreator, cat: ecat, name: ename, date: edate, start: estart, end: eend },
+				data: { cat: ecat, name: ename, date: edate, start: estart, end: eend },
 				success: function(msg){
 					alert(msg);
 				}
 			});
-		}
+		//}
 	});
 });
 
--- addEventScript.php
+++ addEventScript.php
@@ -1,22 +1,24 @@
 <?php
-	if (!isset($_POST['creator']) || !isset($_POST['cat']) || !isset($_POST['name']) || 
+	session_start();
+
+	if (!isset($_POST['cat']) || !isset($_POST['name']) || 
 		!isset($_POST['date']) || !isset($_POST['start']) || !isset($_POST['end'])) {
 		
 		echo "Please enter all fields";
 		exit;
 	}
 	
-	$creator = $_SESSION['creator'];
+	$creator = $_SESSION['username'];
 	$name = $_POST['name'];
-	$month = $_POST['date'];
+	$date = $_POST['date'];
 	$cat = $_POST['cat'];
 	$start = $_POST['start'];
 	$end = $_POST['end'];
 	
 	require "php_database.php";
 					
-	$addEvent = $mysqli -> prepare ("insert into events (creator, cat, name, day, month, year, start, end)
-									values ('$creator', '$cat', '$name', '$day', '$month', '$year', '$start', '$end')");
+	$addEvent = $mysqli -> prepare ("insert into events (creator, cat, name, date, start, end)
+									values ('$creator', '$cat', '$name', '$date', '$start', '$end')");
 					
 	if (!$addEvent) {
 		echo "Insert Query Prep Failed: %s\n", $mysqli -> error;
